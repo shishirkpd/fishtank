5540229bbfb1a8d66c59581501f8c835b1a424b2
shishir kumar <shishir@shishirs-MacBook-Air.local>
2021-01-01 00:29:34 +0800

    updated the popwindow

--- src/components/PopUpWindow.js
+++ src/components/PopUpWindow.js
@@ -1,17 +1,54 @@
 import * as React from 'react';
 
-const PopUpWindow = props => {
+function selectFilter(props) {
+    alert("selected");
+}
 
+function PopUpWindow(props) {
     return (
         <div className="popup-box">
             <div className="box">
                 <div className="close-icon" >
                     <span onClick={props.handleClose}>close X</span>
                 </div>    
+                <div className="productDetailWindowTitle">{props.pageDetails.title}</div>
+                <div className="productDetailWindowSubTitle">{props.pageDetails.subtitle}</div>
+                {props.pageDetails.filter.map(filter => 
+                    <div>
+                        <div className="productDetailsWindowButton" onClick={selectFilter}>
+                            <div className="productDetailsWindowButtonText">{filter.name}</div>
+                            <div className="productDetailsWindowButtonDesc">{filter.description}</div>
+                        </div>
+                    </div> 
+
+                )}    
+                <div className="productDetailsWindowAdditionInfo">{props.pageDetails.pageInfo.additionalInfo}</div>
+                <div className="productDetailsWindowFilterInfo">
+                    {props.pageDetails.pageInfo.filterInfo}
+                    <span className="show">Show</span>
+                </div>
+                <PopUpWindowProductsSection conent={props}/>
             </div>
-            {props.content}
+        </div>
+    );
+   
+};
+
+const PopUpWindowProductsSection = props => {
+    return(
+        <div className="popUpWindowProductsSection">
+            <div className="productPagination">
+                <div style={{'width':'205px', 'float':'left'}}>
+                    items per page: 
+                    {[9, 12, 24, 36].map(num => 
+                        <span style={{'margin-left':'5px'}}>{num}</span>)
+                    }
+                </div>
+                <div style={{'margin-right':'5px', 'float':'right'}}>Next </div>
+            </div>
         </div>
     );
 };
+
 
 export default PopUpWindow;
--- src/components/TankCartBuilder.js
+++ src/components/TankCartBuilder.js
@@ -3,8 +3,37 @@ import MyTank from './MyTank';
 import TankAccessories from './TankAccessories';
 import PopUpWindow from './PopUpWindow';
 
+const testData = {   
+    title: "Choose A Tank",
+    subtitle: "Please choose a tank size",
+    filter: [
+        { 
+            name: "small tank",
+            description: "1 - 10 gallon",
+            isActive: true
+        }, 
+        { 
+            name: "medium tank",
+            description: "10+ - 50 gallon",
+            isActive: false
+        }, 
+        { 
+            name: "large tank",
+            description: "50+ gallon",
+            isActive: false
+        }
+    ],
+    pageInfo: {
+        additionalInfo: "Know which fish you want?",
+        filterInfo: "Filter by fish to find them the right home."
+    },
+    listOfProducts: [
 
-const TankCartBuilder = props =>  {
+    ]
+};
+
+
+function TankCartBuilder(props) {
     const [isOpen, setIsOpen] = useState(false);
  
     const togglePopup = () => {
@@ -14,8 +43,9 @@ const TankCartBuilder = props =>  {
     <div className="tankCartBuilder">
         <MyTank />
         <input  className="chooseButton" type="button" value="choose tank" onClick={togglePopup}/>
-        {isOpen && <PopUpWindow handleClose={togglePopup}
-        />}
+        { isOpen && 
+            <PopUpWindow handleClose={togglePopup} pageDetails={testData}/>
+        }
     </div>
     );
 };
