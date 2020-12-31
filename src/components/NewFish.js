4b7b032e33fcd9555c598150a249613a72155d9d
shishir kumar <shishir@shishirs-MacBook-Air.local>
2021-01-01 00:47:28 +0800

    updated const to function

--- src/components/NewFish.js
+++ src/components/NewFish.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-const NewFish = () =>  {
+function NewFish() {
     return(
     <div className="newFishImage">
         <img src="https://images.pexels.com/photos/433157/pexels-photo-433157.jpeg?auto=compress&cs=tinysrgb&w=550" />
